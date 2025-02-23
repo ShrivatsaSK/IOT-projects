d904c4cea344938b4c9acce94dfd37e1327d0eba
Shrivatsa S K <159104441+ShrivatsaSK@users.noreply.github.com>
2025-02-23 23:39:23 +0530

    Update Interface_code.java

--- Weather Harmony System/Interface_code.java
+++ Weather Harmony System/Interface_code.java
@@ -10,7 +10,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
